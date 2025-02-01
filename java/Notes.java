48d9ac6609e8a37ebbca3dbdad2036ccde75da1a
srirambadisa <srirambadisa1938@gmail.com>
2025-02-01 23:37:05 +0530

    Add files via upload

---
+++ java/Notes.java
@@ -0,0 +1,14 @@
+package a;
+
+public class Notes {
+
+	public static void main(String[] args) {
+		//Q. Give 20% discount on Every bill
+		double bill=2500;
+		double discount=bill*20/100;
+		bill=bill-discount;
+		System.out.println("pay : "+bill);
+
+	}
+
+}

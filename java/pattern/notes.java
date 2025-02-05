3b6f0c0dda1e74295d49e1e95cbdabd4cacb4763
srirambadisa <srirambadisa1938@gmail.com>
2025-02-05 21:54:43 +0530

    Update notes.java

--- java/pattern/notes.java
+++ java/pattern/notes.java
@@ -1,1 +1,9 @@
+                  pattern
+//=> patterns are used to display 2-dimensional information.
+//=> we use nested for loop.
+//=> outer loop represents number of rows.
+//=> inner loop represents number of columns.
 
+for(int i=1 ; i<=5 ; i++)
+  {
+    

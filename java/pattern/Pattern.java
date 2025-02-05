d990ac14f9cf3878dc403521be8c6113d0776292
srirambadisa <srirambadisa1938@gmail.com>
2025-02-05 22:44:40 +0530

    Update Pattern.java

--- java/pattern/Pattern.java
+++ java/pattern/Pattern.java
@@ -24,174 +24,232 @@ Note : code will same for all but must know what we print
 */
 
 
-//Q. print "+" symbol with "*" upto 9 rows & 9 cols
-        *
-        *
-        *
-        *
-* * * * * * * * *
-        *
-        *
-        *
-        *
+package patterns;
 
-for(int i=1 ; i<=9 ; i++)
+public class Pattern 
 {
-	for(int j=1 ; j<=9 ; j++)
+	public static void main(String[] args) 
 	{
-		if(i==5 || j==5)
-			System.out.print("* ");
-		else
-			System.out.print("  ");
+		/* Q. PLUS
+		
+		         	*         
+			        *         
+			        *         
+			        *         
+			* * * * * * * * * 
+			        *         
+			        *         
+			        *         
+			        *
+		 */
+		for(int i=1;i<=9;i++)
+		{
+			for(int j=1;j<=9;j++)
+			{
+				if(i==5 || j==5)
+				{
+					System.out.print("* ");
+				}
+				else
+				{
+					System.out.print("  ");
+				}
+			}
+			System.out.println();
+		}
+		/*	BOX
+		 
+			* * * * * * * * * 
+			*               * 
+			*               * 
+			*               * 
+			*               * 
+			*               * 
+			*               * 
+			*               * 
+			* * * * * * * * * 
+
+		 */
+		for(int i=1;i<=9;i++)
+		{
+			for(int j=1;j<=9;j++)
+			{
+				if(i==1 || i==9 || j==1 || j==9)
+				{
+					System.out.print("* ");
+				}
+				else
+				{
+					System.out.print("  ");
+				}
+			}
+			System.out.println();
+		}
+		/*
+		 	*                 
+			  *               
+			    *             
+			      *           
+			        *         
+			          *       
+			            *     
+			              *   
+			                * 
+		*/
+		for(int i=1;i<=9;i++)
+		{
+			for(int j=1;j<=9;j++)
+			{
+				if(i==j)
+				{
+					System.out.print("* ");
+				}
+				else
+				{
+					System.out.print("  ");
+				}
+			}
+			System.out.println();
+		}
+		
+		/*
+		
+		                * 
+		              *   
+		            *     
+		          *       
+		        *         
+		      *           
+		    *             
+		  *               
+		*                 
+
+		
+		*/
+		for(int i=1;i<=9;i++)
+		{
+			for(int j=1;j<=9;j++)
+			{
+				if(i+j==10)
+				{
+					System.out.print("* ");
+				}
+				else
+				{
+					System.out.print("  ");
+				}
+			}
+			System.out.println();
+		}
+		
+		/*
+		
+		* * * * * * * * * 
+		  *           *   
+		    *       *     
+		      *   *       
+		        *         
+		      *   *       
+		    *       *     
+		  *           *   
+		* * * * * * * * *
+
+		*/
+		for(int i=1;i<=9;i++)
+		{
+			for(int j=1;j<=9;j++)
+			{
+				if(i==1 || i==9 || i==j || i+j==10)
+				{
+					System.out.print("* ");
+				}
+				else
+				{
+					System.out.print("  ");
+				}
+			}
+			System.out.println();
+		}
+		/*
+		
+		* * * * *       * 
+		        *       * 
+		        *       * 
+		        *       * 
+		* * * * * * * * * 
+		*       *         
+		*       *         
+		*       *         
+		*       * * * * * 
+		
+		*/
+		for(int i=1;i<=9;i++)
+		{
+			for(int j=1;j<=9;j++)
+			{
+				if((i==1&&j<=5)||(i==5||j==5)||(j==9&&i<=5)||(j==1&&i>=5)||(i==9&&j>=5))
+				{
+					System.out.print("* ");
+				}
+				else
+				{
+					System.out.print("  ");
+				}
+			}
+			System.out.println();
+		}
+		/*
+		*               * 
+		  *           *   
+		    *       *     
+		      *   *       
+		        *         
+		        *         
+		        *         
+		        *         
+		        *         
+		*/
+		for(int i=1;i<=9;i++)
+		{
+			for(int j=1;j<=9;j++)
+			{
+				if((i==j&&i<=5)||(j==5&&i>=5)||(i+j==10&&i<=5))
+				{
+					System.out.print("* ");
+				}
+				else
+				{
+					System.out.print("  ");
+				}
+			}
+			System.out.println();
+		}
+		/*
+		        *         
+		      *   *       
+		    *       *     
+		  *           *   
+		*               * 
+		  *           *   
+		    *       *     
+		      *   *       
+		        *         
+		*/
+		for(int i=1;i<=9;i++)
+		{
+			for(int j=1;j<=9;j++)
+			{
+				if((i+j==6)||(j-i==4)||(i-j==4)||(i+j==14))
+				{
+					System.out.print("* ");
+				}
+				else
+				{
+					System.out.print("  ");
+				}
+			}
+			System.out.println();
+		}
 	}
-	System.out.println();
 }
-/*
-//Q. box
-* * * * * * * * *
-*		*
-*		*
-*		*
-*		*
-*		*
-*		*
-*		*
-* * * * * * * * *
-//A.
 
-//Q.
-*
-  *
-    *
-      *
-        *
-	  *
-   	    *
-	      *
-		*
-
-//Q. 
-
-                *
-              *
-            *
-          *
-        *
-      *
-    *
-  *
-*
-//Q. 
-	
-* * * * * * * * *
-  *	      *
-    *       *
-      *   *
-        *
-      *	  *
-    *       *
-  *	      *
-* * * * * * * * * 
-//Q. 
-
-* * * * *	*
-	*	*
- 	*	*
-  	*	*
-* * * * * * * * *
-*    	*
-*     	*
-*     	*
-*	* * * * *
-
-//Q. 
-
-*       * * * * * 
-*       *         
-*       *         
-*       *         
-* * * * * * * * * 
-        *       * 
-        *       * 
-        *       * 
-* * * * *       * 
-//Q. 
-
-
-*               * 
-  *           *   
-    *       *     
-      *   *       
-        *         
-        *         
-        *         
-        *         
-        *         
-//Q. 
-
-        *         
-      *   *       
-    *       *     
-  *           *   
-*               * 
-  *           *   
-    *       *     
-      *   *       
-        *         
-
-*/
-package sathya;
-
-public class Pattern {
-
-	public static void main(String[] args) {
-		for(int i=1;i<=9;i++)
-		{
-			for(int j=1;j<=9;j++)
-			{
-				if(j==5 || i==5 || (j==1&&i<=5) || (i==1&&j>=5) || (i==9&&j<=5) || (j==9&&i>=5))
-				{
-					System.out.print("* ");
-				}
-				else
-					System.out.print("  ");
-			}
-			System.out.println();
-		}
-		
-		System.out.println();
-		
-		for(int i=1;i<=9;i++)
-		{
-			for(int j=1;j<=9;j++)
-			{
-				if((i==j&&i<=5) || (i+j==10&&j>=5)|| (j==5&&i>=5))
-				{
-					System.out.print("* ");
-				}
-				else
-					System.out.print("  ");
-			}
-			System.out.println();
-		}
-		
-		System.out.println();
-
-		
-		for(int i=1;i<=9;i++)
-		{
-			for(int j=1;j<=9;j++)
-			{
-				if((i+j==6) || (j-i==4) || (i-j==4) || (i+j==14))
-				{
-					System.out.print("* ");
-				}
-				else
-					System.out.print("  ");
-			}
-			System.out.println();
-		}
-
-	}
-
-}

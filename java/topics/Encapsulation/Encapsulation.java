9747374c605e112cbf3415d5b28bc420a0661ab0
srirambadisa <srirambadisa1938@gmail.com>
2025-02-11 22:15:19 +0530

    Add files via upload

---
+++ java/topics/Encapsulation/Encapsulation.java
@@ -0,0 +1,44 @@
+package Encapsulation;
+/*
+  
+ 
+ => the concept of protecting object information 
+ 	in the process of communication.
+ => we can implement encapsulation using [pojo] rules
+ 
+ POJO : [playing old java object]
+ 
+ 1. class is public
+ 		hence the object is visible to other objects in 
+ 		communication process
+ 2. variables are private
+  		hence are object can not access the information
+  		of other object directly
+ 3. Every variable has setter & getter to share the 
+ 		information
+ 		
+ public class Person
+ {
+ 	private String name;
+ 	
+ 	public void setName(String name)
+ 	{
+ 		this.name=name;
+ 	}
+ 	
+ 	public String getName()
+ 	{
+ 		return this.name;
+ 	}
+ }
+
+
+
+*/
+public class Encapsulation 
+{
+	
+	
+	
+	
+}

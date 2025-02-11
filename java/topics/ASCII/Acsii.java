b6f8f58eaab559d2027abf1609291e90dd192fce
srirambadisa <srirambadisa1938@gmail.com>
2025-02-11 22:05:14 +0530

    Update Acsii.java

--- java/topics/ASCII/Acsii.java
+++ java/topics/ASCII/Acsii.java
@@ -9,9 +9,10 @@ package ASCII;
  	
  	A-65	a-97	0-48	#-35
  	B-66	b-98	1-49	$-36
- 	...		...		...		...
+ 	...	...	...	...
  	Z-90	z-122	9-57	...
- 	
+
+----------------------------------------------------------------
  Q. converting char to int & int to char?
  
  A. System.out.println("ACSII values of upperCase alphabets :");
@@ -24,13 +25,13 @@ package ASCII;
 	{
 		System.out.println(i+" : "+(char)i);
 	}
-
+----------------------------------------------------------------
 	int->char
 	int x=65;
 	char ch=(char)x;
 	
 	upper->lower			
-	char x='A';				A=>65+32=97(a)
+	char x='A';		A=>65+32=97(a)
 	char y=(char)(x+32);	B=>66+32=98(b)
 	
 	char->int
@@ -53,7 +54,7 @@ package ASCII;
 	
 	int sum=(ch1-48)+(ch2-48)+(ch3-48);
 	print(sum);
-	
+----------------------------------------------------------------
  Q. sum of digits in char array ?
  A.		char[] arr = {'a','b','c','@','3','5','6','$'};
 		int sum=0;
@@ -66,7 +67,7 @@ package ASCII;
 		}
 		System.out.println(sum);
 
-
+----------------------------------------------------------------
  Q. difference b/w Strings & Arrays
 
  A.	char[] arr = {'a','b','c','@','3','5','6','$'};
@@ -78,7 +79,7 @@ package ASCII;
 	System.out.println("String length : "+word.length());
 	System.out.println("First char : "+word.charAt(0));
 	System.out.println("last char : "+word.charAt(word.length()-1));
-
+----------------------------------------------------------------
  Q. Display String char by char ?
  
  A. String word="sriram";
@@ -92,28 +93,28 @@ package ASCII;
 	{
 		System.out.println(word.charAt(i));
 	}
-
+----------------------------------------------------------------
  Q. Display Array of Strings ?
  A. String[] arr= {"abc","xyz","pqr","lmn"};
 	for(String x:arr)
 	{
 		System.out.println(x);
 	}
-	
+----------------------------------------------------------------	
  Q. Display length of each String in array
  A. String[] arr= {"c++","Java","python","Android","PHP"};
 	for(String x:arr)
 	{
 		System.out.println(x+" : "+x.length());
 	}
-	
+----------------------------------------------------------------	
  Q. Display first & last character of String in array
  A. String[] arr= {"c++","Java","python","Android","PHP"};
 	for(String x:arr)
 	{
 		System.out.println(x.charAt(0)+" : "+x.charAt(x.length()-1));
 	}
-	
+----------------------------------------------------------------	
  Q. Display last characters of each string in reverse order ?
  A. String[] arr= {"c++","Java","python","Android","PHP"};
 	for(int i=arr.length-1;i>=0;i--)
@@ -121,7 +122,7 @@ package ASCII;
 		String s=arr[i];
 		System.out.println(s.charAt(s.length()-1));
 	}
-	
+----------------------------------------------------------------	
  Q. Display each String in reverse order
  A. String[] arr= {"c++","Java","python","Android","PHP"};
 	for(String s:arr)
@@ -132,7 +133,7 @@ package ASCII;
 		}
 		System.out.println();
 	}
-
+----------------------------------------------------------------
 
 
 */
